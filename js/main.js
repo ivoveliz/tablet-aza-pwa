32501902d893acbb3cfe6ffc0cc026f213fbe511
Ivo Veliz saleh <102048374+ivoveliz@users.noreply.github.com>
2023-09-05 14:31:42 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -3,9 +3,14 @@ const deviceNameLabel = document.getElementById('device-name');
 const connectButton = document.getElementById('connect');
 const disconnectButton = document.getElementById('disconnect');
 const terminalContainer = document.getElementById('terminal');
-const sendForm = document.getElementById('send-form');
-const inputField = document.getElementById('input');
-const inputturno = document.getElementById('turno');
+const sendButton = document.getElementById('enviar'); // Cambiado el ID
+const inputField = document.getElementById('aceite'); // Cambiado el ID
+const inputTurno = document.getElementById('turno');
+const inputCombustible = document.getElementById('combustible');
+const inputKilometraje = document.getElementById('kilometraje');
+const inputLimpieza = document.querySelectorAll('input[name="limpieza"]');
+const inputInicioTurno = document.getElementById('inicio-turno');
+const inputFinTurno = document.getElementById('fin-turno');
 
 // Helpers.
 const defaultDeviceName = 'Terminal';
@@ -39,28 +44,27 @@ terminal.receive = function(data) {
 
 // Override default log method to output messages to the terminal and console.
 terminal._log = function(...messages) {
-  // We can't use `super._log()` here.
   messages.forEach((message) => {
     logToTerminal(message);
-    console.log("mensaje",message); // eslint-disable-line no-console
+    console.log("mensaje", message); // eslint-disable-line no-console
   });
 };
 
-// Implement own send function to log outcoming data to the terminal.
+// Implement own send function to log outgoing data to the terminal.
 const send = (data) => {
-  console.log(data)
-  terminal.send(data).
-      then(() => logToTerminal(data, 'out')).
-      catch((error) => logToTerminal(error));
+  console.log(data);
+  terminal.send(data)
+    .then(() => logToTerminal(data, 'out'))
+    .catch((error) => logToTerminal(error));
 };
 
 // Bind event listeners to the UI elements.
 connectButton.addEventListener('click', () => {
-  terminal.connect().
-      then(() => {
-        deviceNameLabel.textContent = terminal.getDeviceName() ?
-            terminal.getDeviceName() : defaultDeviceName;
-      });
+  terminal.connect()
+    .then(() => {
+      deviceNameLabel.textContent = terminal.getDeviceName() ?
+        terminal.getDeviceName() : defaultDeviceName;
+    });
 });
 
 disconnectButton.addEventListener('click', () => {
@@ -68,29 +72,38 @@ disconnectButton.addEventListener('click', () => {
   deviceNameLabel.textContent = defaultDeviceName;
 });
 
-sendForm.addEventListener('submit', (event) => {
-  event.preventDefault();
- 
+sendButton.addEventListener('click', () => { // Cambiado el evento de 'submit' a 'click'
   const valor = inputField.value;
-  const turno = inputturno.value;
+  const turno = inputTurno.value;
+  const combustible = inputCombustible.value;
+  const kilometraje = inputKilometraje.value;
+  const limpieza = [...inputLimpieza].find((radio) => radio.checked)?.value || '';
+  const inicioTurno = inputInicioTurno.value;
+  const finTurno = inputFinTurno.value;
 
-  // Combina los valores en un formato que desees enviar.
-  const dataToSend = `${turno}, ${valor}`;
+  // Combina los valores en el formato que deseas enviar.
+  const dataToSend = `Turno: ${turno}, Aceite: ${valor}, Combustible: ${combustible}, Kilometraje: ${kilometraje}, Limpieza: ${limpieza}, Inicio de turno: ${inicioTurno}, Fin de turno: ${finTurno}`;
 
   send(dataToSend);
 
   // Limpia los campos después de enviar.
   inputField.value = '';
-  inputturno.value = '';
+  inputTurno.value = 'A'; // Puedes establecer un valor predeterminado si lo deseas.
+  inputCombustible.value = '';
+  inputKilometraje.value = '';
+  inputLimpieza.forEach((radio) => {
+    radio.checked = false;
+  });
+  inputInicioTurno.value = '';
+  inputFinTurno.value = '';
 
   inputField.focus();
-  inputturno.focus();
 });
 
-// Switch terminal auto scrolling if it scrolls out of bottom.
+// Switch terminal auto-scrolling if it scrolls out of the bottom.
 terminalContainer.addEventListener('scroll', () => {
   const scrollTopOffset = terminalContainer.scrollHeight -
-      terminalContainer.offsetHeight - terminalAutoScrollingLimit;
+    terminalContainer.offsetHeight - terminalAutoScrollingLimit;
 
   isTerminalAutoScrolling = (scrollTopOffset < terminalContainer.scrollTop);
 });
