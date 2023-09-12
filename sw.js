3b7e9565f883ea156e299ab1577753edf100706f
Ivo Veliz saleh <102048374+ivoveliz@users.noreply.github.com>
2023-09-12 11:44:13 -0300

    cambios

--- sw.js
+++ sw.js
@@ -28,15 +28,11 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) {
-        return response;
+        return response; // Devuelve la respuesta en caché si está disponible
       }
-      return fetch(event.request).then((response) => {
-        const clonedResponse = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, clonedResponse);
-        });
-        return response;
-      });
+      // Si no hay una respuesta en caché, no intentes recuperarla en línea
+      // Devuelve una respuesta vacía o una página de error personalizada
+      return new Response('Esta página no está disponible en línea. Por favor, consulta la versión offline.');
     })
   );
 });
