eb6f6c22ba954f1e289cce7c82526bcbb8770686
Ivo Veliz saleh <102048374+ivoveliz@users.noreply.github.com>
2023-09-11 23:37:42 -0300

    cambios

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 importScripts('js/sw-toolbox.js');
 
-toolbox.precache([
+const CACHE_NAME = 'pwa-cache-v1'; // Nombre de la caché
+const CACHE_URLS = [
+  '/',
   'css/normalize.css',
   'css/styles.css',
   'js/BluetoothConnection.js',
@@ -9,15 +11,22 @@ toolbox.precache([
   'index.html',
   'terminal.html',
   'checklist.html',
-]);
+  // Agrega aquí más recursos que deseas almacenar en caché
+];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_URLS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          return caches.delete(cacheName);
-        })
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
       );
     })
   );
@@ -26,7 +35,14 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      // Intenta obtener el recurso desde la caché
+      return response || fetch(event.request).then((fetchResponse) => {
+        // Almacena el recurso recién recuperado en la caché
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
     })
   );
 });
